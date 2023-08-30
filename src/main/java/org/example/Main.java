f01b47587f3db581abaa7bca8677eb3546f8be2f
Silas <SilasTompkins@outlook.com>
2023-08-30 10:29:44 -0400

    git init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+
+class Index implements HttpHandler {
+    private final String input;
+
+    public Index(String input) {
+        this.input = input;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException
+    {
+        System.out.println("Got " + input + " request");
+        byte[] test = "test".getBytes();
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, test.length);
+        exchange.getResponseHeaders().add("Content-Type", "text/html");
+        exchange.getResponseBody().write(test);
+        exchange.close();
+        System.out.println("Closed");
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+        HttpServer server = HttpServer.create(new InetSocketAddress(5678), 0);
+        server.createContext("/", new Index("/"));
+        server.createContext("/test", new Index("test"));
+        server.start();
+    }
+}
