4e28d0f6a141cf210111f78691faebe3e11e06a8
stompkins9 <silas.tompkins@gtri.gatech.edu>
2023-08-31 16:20:11 -0400

    form query now parses into map<string, string>

--- src/main/java/org/example/Routes.java
+++ src/main/java/org/example/Routes.java
@@ -3,18 +3,24 @@ package org.example;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import sun.nio.ch.IOUtil;
-import sun.nio.cs.StandardCharsets;
+//import sun.nio.ch.IOUtil;
+//import sun.nio.cs.StandardCharsets;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Scanner;
 
 class Index implements HttpHandler {
     private final String input;
@@ -83,15 +89,26 @@ class Second implements HttpHandler {
 }
 
 class Form implements HttpHandler {
+    public static Map<String, String> splitQuery(String query) throws UnsupportedEncodingException {
+        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+                    URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+        }
+        return query_pairs;
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         System.out.println("got /form request");
         exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
         exchange.getResponseHeaders().add("Content-Type", "text/html");
-        InputStream test = exchange.getRequestBody();
-        String result = IOUtil.toString(test,StandardCharsets.UTF_8);
+        byte[] query = exchange.getRequestBody().readAllBytes();
+        Map<String, String> query_map = splitQuery(new String(query, StandardCharsets.UTF_8));
+        System.out.println(query_map);
         exchange.close();
-        test.close();
         System.out.println("Closed");
     }
 }
