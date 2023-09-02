4bd3582f4cdc8cc40966150bfd6169b9de8407f5
seesharpsi <silastompkins@outlook.com>
2023-09-01 21:12:54 -0400

    .idea stuff added, added run.sh script

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo /home/silas/.jdks/openjdk-20.0.2/bin/java -javaagent:/home/silas/.cache/JetBrains/RemoteDev/dist/5b92388135cf0_ideaIU-2023.2.1/lib/idea_rt.jar=38281:/home/silas/.cache/JetBrains/RemoteDev/dist/5b92388135cf0_ideaIU-2023.2.1/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/silas/projects/java/java-htmx/target/classes:/home/silas/.m2/repository/org/xerial/sqlite-jdbc/3.43.0.0/sqlite-jdbc-3.43.0.0.jar org.example.Main
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,11 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -17,5 +22,35 @@ public class Main {
         server.createContext("/second", new Second());
         server.createContext("/form", new Form());
         server.start();
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:/home/silas/projects/java/java-htmx/test.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30); // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            while (rs.next()) {
+                // read the result set
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
