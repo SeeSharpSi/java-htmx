0194b9afa711d709f3c32cdeb157e89e14ef1cfc
GrandStrategos <joshgree747@gmail.com>
2023-11-26 16:09:53 -0500

    SQLController

--- src/main/java/org/example/SQLController.java
+++ src/main/java/org/example/SQLController.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+//Class written by Joshua Green
 public class SQLController {
 
     private Connection connect() {
