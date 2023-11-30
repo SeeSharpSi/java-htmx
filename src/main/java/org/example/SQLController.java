711a91fc180dca5147b7cd8d79a12169120879f1
GrandStrategos <joshgree747@gmail.com>
2023-11-30 13:27:35 -0500

    Adder and deleter methods

--- src/main/java/org/example/SQLController.java
+++ src/main/java/org/example/SQLController.java
@@ -20,8 +20,8 @@ public class SQLController {
         return conn;
     }
 
-    public void addCustomer(int id, String name, String username, String password, String address, int cardNumber, int cardSVC, int ordersDelivered, int accessLevel) {
-        String customer = "INSERT INTO pizzaToppings(id, name, username, password, address, cardNumber, cardSVC, ordersDelivered, accessLevel) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    public void addTopping(int id, String name, float price) {
+        String pizzaTopping = "INSERT INTO pizzaToppings(id, name, price) VALUES(?, ?, ?)";
 
         Connection conn = null;
         PreparedStatement ptsmt = null;
@@ -34,16 +34,10 @@ public class SQLController {
 
             conn.setAutoCommit(false);
 
-            ptsmt = conn.prepareStatement(customer, Statement.RETURN_GENERATED_KEYS);
+            ptsmt = conn.prepareStatement(pizzaTopping, Statement.RETURN_GENERATED_KEYS);
             ptsmt.setInt(1, id);
             ptsmt.setString(2, name);
-            ptsmt.setString(3, username);
-            ptsmt.setString(4, password);
-            ptsmt.setString(5, address);
-            ptsmt.setInt(6, cardNumber);
-            ptsmt.setInt(7, cardSVC);
-            ptsmt.setInt(8, ordersDelivered);
-            ptsmt.setInt(9, accessLevel);
+            ptsmt.setFloat(3, price);
 
             ptsmt.executeUpdate();
 
@@ -103,7 +97,7 @@ public class SQLController {
         } finally {
             try {
                 if (ptsmt != null) {
-                   ptsmt.close();
+                    ptsmt.close();
                 }
                 if (conn != null) {
                     conn.close();
@@ -114,8 +108,8 @@ public class SQLController {
         }
     }
 
-    public void addTopping(int id, String name, float price) {
-        String pizzaTopping = "INSERT INTO pizzaToppings(id, name, price) VALUES(?, ?, ?)";
+    public void createAdmin(int id, String username, String password, String status) {
+        String admin = "INSERT INTO adminUserProfile(id, username, password, status) VALUES(?, ?, ?, ?)";
 
         Connection conn = null;
         PreparedStatement ptsmt = null;
@@ -128,10 +122,11 @@ public class SQLController {
 
             conn.setAutoCommit(false);
 
-            ptsmt = conn.prepareStatement(pizzaTopping, Statement.RETURN_GENERATED_KEYS);
+            ptsmt = conn.prepareStatement(admin, Statement.RETURN_GENERATED_KEYS);
             ptsmt.setInt(1, id);
-            ptsmt.setString(2, name);
-            ptsmt.setFloat(3, price);
+            ptsmt.setString(2, username);
+            ptsmt.setString(3, password);
+            ptsmt.setString(4, status);
 
             ptsmt.executeUpdate();
 
@@ -160,5 +155,230 @@ public class SQLController {
             }
         }
 
+    }
+
+    public void deleteAdmin(int id) {
+        String deleteAdmin = "DELETE FROM adminUserProfile where id = ?";
+
+        Connection conn = null;
+        PreparedStatement ptsmt = null;
+
+        try {
+            conn = this.connect();
+            if (conn == null) {
+                return;
+            }
+
+            conn.setAutoCommit(false);
+
+            ptsmt = conn.prepareStatement(deleteAdmin, Statement.RETURN_GENERATED_KEYS);
+            ptsmt.setInt(1, id);
+            ptsmt.executeUpdate();
+        } catch (SQLException e1) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e2) {
+                System.out.println(e2.getMessage());
+            }
+            System.out.println(e1.getMessage());
+        } finally {
+            try {
+                if (ptsmt != null) {
+                    ptsmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e3) {
+                System.out.println(e3.getMessage());
+            }
+        }
+    }
+
+    public void addCustomer(int id, String name, String username, String password, String address, int cardNumber, int cardSVC, int ordersDelivered, int accessLevel) {
+        String customer = "INSERT INTO pizzaToppings(id, name, username, password, address, cardNumber, cardSVC, ordersDelivered, accessLevel) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement ptsmt = null;
+
+        try {
+            conn = this.connect();
+            if (conn == null) {
+                return;
+            }
+
+            conn.setAutoCommit(false);
+
+            ptsmt = conn.prepareStatement(customer, Statement.RETURN_GENERATED_KEYS);
+            ptsmt.setInt(1, id);
+            ptsmt.setString(2, name);
+            ptsmt.setString(3, username);
+            ptsmt.setString(4, password);
+            ptsmt.setString(5, address);
+            ptsmt.setInt(6, cardNumber);
+            ptsmt.setInt(7, cardSVC);
+            ptsmt.setInt(8, ordersDelivered);
+            ptsmt.setInt(9, accessLevel);
+
+            ptsmt.executeUpdate();
+
+            conn.commit();
+
+        } catch (SQLException e1) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e2) {
+                System.out.println(e2.getMessage());
+            }
+            System.out.println(e1.getMessage());
+
+        } finally {
+            try {
+                if (ptsmt != null) {
+                    ptsmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e3) {
+                System.out.println(e3.getMessage());
+            }
+        }
+
+    }
+
+    public void deleteCustomer (int id) {
+        String deleteCustomer = "DELETE FROM customerUserProfile where id = ?";
+
+        Connection conn = null;
+        PreparedStatement ptsmt = null;
+
+        try {
+            conn = this.connect();
+            if (conn == null) {
+                return;
+            }
+
+            conn.setAutoCommit(false);
+
+            ptsmt = conn.prepareStatement(deleteCustomer, Statement.RETURN_GENERATED_KEYS);
+            ptsmt.setInt(1, id);
+            ptsmt.executeUpdate();
+        } catch (SQLException e1) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e2) {
+                System.out.println(e2.getMessage());
+            }
+            System.out.println(e1.getMessage());
+        } finally {
+            try {
+                if (ptsmt != null) {
+                    ptsmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e3) {
+                System.out.println(e3.getMessage());
+            }
+        }
+    }
+
+    public void addEmployee(int id, String username, String password, String status, int ordersDelivered, int accessLevel) {
+        String employee = "INSERT INTO employeeUserProfile(id, username, password, status, ordersDelivered, accessLevel) VALUES(?, ?, ?, ?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement ptsmt = null;
+
+        try {
+            conn = this.connect();
+            if (conn == null) {
+                return;
+            }
+
+            conn.setAutoCommit(false);
+
+            ptsmt = conn.prepareStatement(employee, Statement.RETURN_GENERATED_KEYS);
+            ptsmt.setInt(1, id);
+            ptsmt.setString(2, username);
+            ptsmt.setString(3, password);
+            ptsmt.setString(4, status);
+            ptsmt.setInt(5, ordersDelivered);
+            ptsmt.setInt(6, accessLevel);
+
+            ptsmt.executeUpdate();
+
+            conn.commit();
+
+        } catch (SQLException e1) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e2) {
+                System.out.println(e2.getMessage());
+            }
+            System.out.println(e1.getMessage());
+
+        } finally {
+            try {
+                if (ptsmt != null) {
+                    ptsmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e3) {
+                System.out.println(e3.getMessage());
+            }
+        }
+
+    }
+
+    public void deleteEmployee(int id) {
+        String deleteEmployee = "DELETE FROM customerUserProfile where id = ?";
+
+        Connection conn = null;
+        PreparedStatement ptsmt = null;
+
+        try {
+            conn = this.connect();
+            if (conn == null) {
+                return;
+            }
+
+            conn.setAutoCommit(false);
+
+            ptsmt = conn.prepareStatement(deleteEmployee, Statement.RETURN_GENERATED_KEYS);
+            ptsmt.setInt(1, id);
+            ptsmt.executeUpdate();
+        } catch (SQLException e1) {
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e2) {
+                System.out.println(e2.getMessage());
+            }
+            System.out.println(e1.getMessage());
+        } finally {
+            try {
+                if (ptsmt != null) {
+                    ptsmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e3) {
+                System.out.println(e3.getMessage());
+            }
+        }
     }
 }
